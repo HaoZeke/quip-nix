9c60f87f4f5a386e0809ac575aadc50aecf9af4c
Rohit Goswami <rgoswami@ieee.org>
2020-08-17 02:47:45 +0000

    shell: Allow project local pip installations
    As seen here:
    https://churchman.nl/2019/01/22/using-nix-to-create-python-virtual-environments/

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 # Define
 let
+  # https://discourse.nixos.org/t/how-to-create-a-nix-shell-environment-with-different-python-version-as-default/3236/3
   overlay = (self: super: rec {
     python38 = super.python38.override {
       packageOverrides = self: super: {
@@ -18,12 +19,19 @@ let
   # Import
   buildpkgs = import ./nix { };
   # Shell Hook
+  # https://churchman.nl/2019/01/22/using-nix-to-create-python-virtual-environments/
   hook = ''
-    export QUIP_ARCH=linux_x86_64_gfortran
-    export QUIP_INSTALLDIR=$PWD/out/bin
-    export QUIP_STRUCTS_DIR=$PWD/structs
-    export PATH=$PATH:$QUIP_INSTALLDIR
+    # QUIP Stuff
+     export QUIP_ARCH=linux_x86_64_gfortran
+     export QUIP_INSTALLDIR=$PWD/out/bin
+     export QUIP_STRUCTS_DIR=$PWD/structs
+     export PATH=$PATH:$QUIP_INSTALLDIR
+    # Python Stuff
+     export PIP_PREFIX="$(pwd)/_build/pip_packages"
+     export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.8/site-packages:$PYTHONPATH"
+     unset SOURCE_DATE_EPOCH
   '';
+  # Apparently pip needs 1980 or above
 in pkgs.mkShell {
   buildInputs = with pkgs; [
     # Required for the shell
