a9a30b7a9ddc89803b978259a1da05a5406b1943
Rohit Goswami <rgoswami@ieee.org>
2020-09-03 14:24:23 +0000

    shell: Refactor as per JuPyNix

--- shell.nix
+++ shell.nix
@@ -6,10 +6,6 @@ let
   inherit (pkgs.lib) optional optionals;
   # Import
   buildpkgs = import ./nix { };
-  libuv = libuv.overrideAttrs (oldAttrs: {
-    doCheck = false;
-    doInstallCheck = false;
-  });
   # Shell Hook
   # https://churchman.nl/2019/01/22/using-nix-to-create-python-virtual-environments/
   # https://discourse.nixos.org/t/how-to-create-a-nix-shell-environment-with-different-python-version-as-default/3236/3
@@ -25,64 +21,61 @@ let
      unset SOURCE_DATE_EPOCH
     # quippy Stuff
      export QUIPPY_INSTALL_OPTS="--prefix $PIP_PREFIX"
-    # Fixes for locale
-     export LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive";
-     export LC_ALL="en_US.UTF-8";
   '';
-  # Apparently pip needs 1980 or above
-  # https://github.com/ento/elm-doc/blob/master/shell.nix
-  python = pkgs."python${pythonVersion}".override {
-    packageOverrides = self: super: {
-      pytest = super.pytest.overridePythonAttrs (old: rec {
-        doCheck = false;
-        doInstallCheck = false;
-      });
-      numpy = super.numpy.overridePythonAttrs (old: rec {
-        doCheck = false;
-        doInstallCheck = false;
-      });
-      scipy = super.scipy.overridePythonAttrs (old: rec {
-        doCheck = false;
-        doInstallCheck = false;
-      });
-      ipython = super.ipython.overridePythonAttrs (old: rec {
-        doCheck = false;
-        doInstallCheck = false;
-      });
-      ipykernel = super.ipykernel.overridePythonAttrs (old: rec {
-        doCheck = false;
-        doInstallCheck = false;
-      });
-      ase = super.ase.overridePythonAttrs (old: rec {
-        doCheck = false;
-        doInstallCheck = false;
-      });
-      f90wrap = self.buildPythonPackage rec {
-        pname = "f90wrap";
-        version = "0.2.3";
-        src = pkgs.fetchFromGitHub {
-          owner = "jameskermode";
-          repo = "f90wrap";
-          rev = "master";
-          sha256 = "0d06nal4xzg8vv6sjdbmg2n88a8h8df5ajam72445mhzk08yin23";
+
+  mach-nix = import (builtins.fetchGit {
+    url = "https://github.com/DavHau/mach-nix/";
+    ref = "2.2.2";
+  });
+  customPython = mach-nix.mkPython {
+    pypi_deps_db_commit = "b5cd0ee30c9a3e4f076374fc4c90cdc4e5f17d3c";
+    pypi_deps_db_sha256 =
+      "0gkklm0zb3pjxkkvvsda0ckvxb54mnqyxz31pwcjxjpx9jlqlbld";
+    requirements = ''
+      f90wrap
+      ase
+      snakemake
+      matplotlib
+      scipy
+      pandas
+      numpy
+      ipython
+      ipykernel
+      pip
+    '';
+    providers = { pytest = "nixpkgs"; };
+    overrides_pre = [
+      (pythonSelf: pythonSuper: {
+        pytest = pythonSuper.pytest.overrideAttrs (oldAttrs: {
+          doCheck = false;
+          doInstallCheck = false;
+        });
+        f90wrap = pythonSelf.buildPythonPackage rec {
+          pname = "f90wrap";
+          version = "0.2.3";
+          src = pkgs.fetchFromGitHub {
+            owner = "jameskermode";
+            repo = "f90wrap";
+            rev = "master";
+            sha256 = "0d06nal4xzg8vv6sjdbmg2n88a8h8df5ajam72445mhzk08yin23";
+          };
+          buildInputs = with pkgs; [ gfortran stdenv ];
+          propagatedBuildInputs = with pythonSelf; [
+            setuptools
+            setuptools-git
+            wheel
+            numpy
+          ];
+          preConfigure = ''
+            export F90=${pkgs.gfortran}/bin/gfortran
+          '';
+          doCheck = false;
+          doIstallCheck = false;
         };
-        buildInputs = with pkgs; [ gfortran stdenv ];
-        propagatedBuildInputs = with self; [
-          setuptools
-          setuptools-git
-          wheel
-          numpy
-        ];
-        preConfigure = ''
-          export F90=${pkgs.gfortran}/bin/gfortran
-        '';
-        doCheck = false;
-        doInstallCheck = false;
-      };
-    };
+      })
+    ];
+    pkgs = pkgs;
   };
-  myPy = python.withPackages
-    (p: with p; [ ase ipython ipykernel scipy numpy pip pandas f90wrap ]);
 in pkgs.mkShell {
   buildInputs = with pkgs; [
     # Required for the shell
@@ -98,9 +91,8 @@ in pkgs.mkShell {
     gcc9
     gfortran
     openblas
-    glibcLocales
 
-    myPy
+    customPython
     # https://github.com/sveitser/i-am-emotion/blob/294971493a8822940a153ba1bf211bad3ae396e6/gpt2/shell.nix
   ];
   shellHook = hook;
