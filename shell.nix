456b0401209b7b163c6d737a30fe99e7e26f6dfe
Rohit Goswami <rgoswami@ieee.org>
2020-09-13 03:14:13 +0000

    shell: Add quip and fix f90wrap

--- shell.nix
+++ shell.nix
@@ -12,8 +12,6 @@ let
   hook = ''
     # QUIP Stuff
      export QUIP_ARCH=linux_x86_64_gfortran
-     export QUIP_INSTALLDIR=$PWD/out/bin
-     export QUIP_STRUCTS_DIR=$PWD/structs
      export PATH=$PATH:$QUIP_INSTALLDIR
     # Python Stuff
      export PIP_PREFIX="$(pwd)/_build/pip_packages"
@@ -26,26 +24,91 @@ let
     doCheck = false;
     doInstallCheck = false;
   });
+  quip = pkgs.stdenv.mkDerivation {
+    name = "quip";
+    src = pkgs.lib.cleanSource ./QUIP;
+    buildInputs = with pkgs; [ gfortran openblas gcc9 ];
+
+    preConfigure = ''
+      export QUIP_ARCH=linux_x86_64_gfortran
+      export QUIP_INSTALLDIR=$out/bin
+      export QUIP_STRUCTS_DIR=$PWD/structs
+      mkdir -p build/$QUIP_ARCH
+      mkdir -p $QUIP_INSTALLDIR
+      cp Makefile.inc build/$QUIP_ARCH
+    '';
+
+    buildPhase = ''
+      make
+    '';
+    installPhase = ''
+      make install
+    '';
+    meta = with pkgs.stdenv.lib; {
+      description = "";
+      longDescription = "";
+      homepage = "";
+      license = licenses.gpl3Plus;
+      platforms = [ "x86_64-linux" ];
+      maintainers = [ maintainers.HaoZeke ];
+    };
+  };
+  f90wrap = mach-nix.buildPythonPackage {
+    pname = "f90wrap";
+    version = "0.2.3";
+    src = pkgs.fetchFromGitHub {
+      owner = "jameskermode";
+      repo = "f90wrap";
+      rev = "master";
+      sha256 = "0d06nal4xzg8vv6sjdbmg2n88a8h8df5ajam72445mhzk08yin23";
+    };
+    buildInputs = with pkgs; [ gfortran stdenv ];
+    requirements = ''
+      numpy
+      setuptools
+      setuptools-git
+      wheel
+    '';
+    propagatedBuildInputs = with mach-nix.python; [
+      setuptools
+      setuptools-git
+      wheel
+      numpy
+    ];
+    preConfigure = ''
+      export F90=${pkgs.gfortran}/bin/gfortran
+    '';
+    doCheck = false;
+    doIstallCheck = false;
+  };
+  # quippy = mach-nix.buildPythonPackage {
+  #   name = "quippy";
+  #   src = ./QUIP;
+  #   # _.buildInputs = with pkgs; [ quip gfortran openblas gcc9 ];
+
+  #   preConfigure = ''
+  #     export QUIP_ARCH=linux_x86_64_gfortran
+  #     export QUIP_INSTALLDIR=$out/bin
+  #     export QUIP_STRUCTS_DIR=$PWD/structs
+  #     mkdir -p build/$QUIP_ARCH
+  #     mkdir -p $QUIP_INSTALLDIR
+  #     cp Makefile.inc build/$QUIP_ARCH
+  #   '';
+
+  #   buildPhase = ''
+  #     make install-quippy
+  #   '';
+  # };
   mach-nix = import (builtins.fetchGit {
     url = "https://github.com/DavHau/mach-nix/";
-    ref = "2.2.2";
+    ref = "2.3.0";
   });
   customPython = mach-nix.mkPython {
-    pypi_deps_db_commit = "b5cd0ee30c9a3e4f076374fc4c90cdc4e5f17d3c";
+    pypi_deps_db_commit = "ed2da0e9bd68cf7050c44f874c54f924675a61b5";
     pypi_deps_db_sha256 =
       "0gkklm0zb3pjxkkvvsda0ckvxb54mnqyxz31pwcjxjpx9jlqlbld";
-    requirements = ''
-      f90wrap
-      ase
-      snakemake
-      matplotlib
-      scipy
-      pandas
-      numpy
-      ipython
-      ipykernel
-      pip
-    '';
+    requirements = builtins.readFile ./requirements.txt;
+    extra_pkgs = [ f90wrap ];
     providers = { pytest = "nixpkgs"; };
     overrides_pre = [
       (pythonSelf: pythonSuper: {
@@ -53,32 +116,10 @@ let
           doCheck = false;
           doInstallCheck = false;
         });
-        f90wrap = pythonSelf.buildPythonPackage rec {
-          pname = "f90wrap";
-          version = "0.2.3";
-          src = pkgs.fetchFromGitHub {
-            owner = "jameskermode";
-            repo = "f90wrap";
-            rev = "master";
-            sha256 = "0d06nal4xzg8vv6sjdbmg2n88a8h8df5ajam72445mhzk08yin23";
-          };
-          buildInputs = with pkgs; [ gfortran stdenv ];
-          propagatedBuildInputs = with pythonSelf; [
-            setuptools
-            setuptools-git
-            wheel
-            numpy
-          ];
-          preConfigure = ''
-            export F90=${pkgs.gfortran}/bin/gfortran
-          '';
-          doCheck = false;
-          doIstallCheck = false;
-        };
+        pkgs = pkgs;
+        disable_checks = true;
       })
     ];
-    pkgs = import sources.nixpkgs { };
-    disable_checks = true;
   };
 in pkgs.mkShell {
   buildInputs = with pkgs; [
@@ -87,7 +128,6 @@ in pkgs.mkShell {
     perl
     git
     direnv
-    fzf
     ag
     fd
 
@@ -101,3 +141,36 @@ in pkgs.mkShell {
   ];
   shellHook = hook;
 }
+
+# extra_pkgs = [
+#   mach-nix.buildPythonPackage
+#   rec {
+#     name = "quippy";
+#     src = ./QUIP;
+#     buildInputs = [ quip ];
+#     doCheck = true;
+#     preConfigure = ''
+#       export QUIP_ARCH=linux_x86_64_gfortran
+#       export QUIP_INSTALLDIR=$out/bin
+#       export QUIP_STRUCTS_DIR=$PWD/structs
+#       mkdir -p build/$QUIP_ARCH
+#       mkdir -p $QUIP_INSTALLDIR
+#       cp Makefile.inc build/$QUIP_ARCH
+#     '';
+#     buildPhase = ''
+#       make install-quippy
+#     '';
+#   }
+# ];
+# _.f90wrap.buildInputs = with pkgs; [ gfortran stdenv ];
+# _.f90wrap.propagatedBuildInputs = with pkgs.python3Packages; [
+#   setuptools
+#   setuptools-git
+#   wheel
+#   numpy
+# ];
+# preConfigure = ''
+#   export F90=${pkgs.gfortran}/bin/gfortran
+# '';
+# doCheck = false;
+# doIstallCheck = false;
