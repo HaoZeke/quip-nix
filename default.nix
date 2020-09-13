509b4968aeec2c9c22d54787c23f26f7645f4531
Rohit Goswami <rgoswami@ieee.org>
2020-09-13 18:30:58 +0000

    default: Init a build compatible setup

---
+++ default.nix
@@ -0,0 +1,75 @@
+let
+
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+  quip = pkgs.callPackage ./pkgs/quip.nix { pythonEnv = customPython; };
+  inherit (pkgs.lib) optional optionals;
+
+  hook = ''
+    # QUIP Stuff
+     export QUIP_ARCH=linux_x86_64_gfortran
+     export QUIP_INSTALLDIR=$PWD/out/bin
+     export QUIP_STRUCTS_DIR=$PWD/structs
+     export PATH=$PATH:$QUIP_INSTALLDIR
+    # Python Stuff
+     export PIP_PREFIX="$(pwd)/_build/pip_packages"
+     export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.8/site-packages"
+     unset SOURCE_DATE_EPOCH
+    # quippy Stuff
+     export QUIPPY_INSTALL_OPTS="--prefix $PIP_PREFIX"
+  '';
+
+  f90wrap = mach-nix.buildPythonPackage {
+    pname = "f90wrap";
+    version = "0.2.3";
+    src = pkgs.fetchFromGitHub {
+      owner = "jameskermode";
+      repo = "f90wrap";
+      rev = "master";
+      sha256 = "0d06nal4xzg8vv6sjdbmg2n88a8h8df5ajam72445mhzk08yin23";
+    };
+    buildInputs = with pkgs; [ gfortran stdenv ];
+    requirements = ''
+      numpy
+      setuptools
+      setuptools-git
+      wheel
+    '';
+    propagatedBuildInputs = with mach-nix.python; [
+      setuptools
+      setuptools-git
+      wheel
+      numpy
+    ];
+    preConfigure = ''
+      export F90=${pkgs.gfortran}/bin/gfortran
+    '';
+    doCheck = false;
+    doIstallCheck = false;
+  };
+  mach-nix = import (builtins.fetchGit {
+    url = "https://github.com/DavHau/mach-nix/";
+    ref = "2.3.0";
+  });
+  customPython = mach-nix.mkPython {
+    pypi_deps_db_commit = "ed2da0e9bd68cf7050c44f874c54f924675a61b5";
+    pypi_deps_db_sha256 =
+      "0gkklm0zb3pjxkkvvsda0ckvxb54mnqyxz31pwcjxjpx9jlqlbld";
+    requirements = builtins.readFile ./requirements.txt;
+    extra_pkgs = [ f90wrap ];
+    providers = { pytest = "nixpkgs"; };
+    overrides_pre = [
+      (pythonSelf: pythonSuper: {
+        pytest = pythonSuper.pytest.overrideAttrs (oldAttrs: {
+          doCheck = false;
+          doInstallCheck = false;
+        });
+        pkgs = pkgs;
+        disable_checks = true;
+      })
+    ];
+  };
+in pkgs.buildEnv {
+  name = "quip-env";
+  paths = [ quip ];
+}
